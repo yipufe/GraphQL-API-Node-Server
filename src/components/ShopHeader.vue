d81ef547c24750ef9d154d2fb75f6b04210b66f3
= <rdmanwill@gmail.com>
2020-02-21 21:39:22 -0700

    Some styling applied to header

--- src/components/ShopHeader.vue
+++ src/components/ShopHeader.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <h1>Header</h1>
+  <div id="header">
+    <h1>BBD</h1>
+    <p>Beter Bottle Distributor</p>
   </div>
 </template>
 
@@ -15,5 +16,12 @@ export default {
 </script>
 
 <style scoped>
+  #header {
+    background: rgb(173,208,255);
+    background: linear-gradient(178deg, rgba(173,208,255,1) 0%, rgba(9,9,121,1) 25%, rgba(4,113,198,1) 81%, rgba(0,190,255,1) 100%);
+    border-bottom: 4px solid #443;
+  }
+  #header h1 {
 
+  }
 </style>
